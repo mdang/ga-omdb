e8128f3f04ed7a6ad1216a09b8090fa0adeb3349
Mike Dang <mike@packstack.co>
2015-03-24 10:54:45 -0500

    Removed the sample search data

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,13 @@
 var omdbControllers = angular.module('omdbControllers', []);
 
 omdbControllers.controller('ListCtrl', function($scope, $http) {
+	
+	/*
 	$http.get('http://www.omdbapi.com/?s=shrek').success(function(data) {
     console.log('data: ', data.Search);
     $scope.movies = data.Search;
   });
+*/
 });
 
 omdbControllers.controller('DetailCtrl', function($scope, $http, $routeParams) {
